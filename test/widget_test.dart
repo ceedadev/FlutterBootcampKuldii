dd20e88c997ea6e9b26068a934f0cc2a0b303afc
Armanda Arif <ceedadev@gmail.com>
2021-11-17 18:02:55 +0700

    modus done

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,66 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:bootcamp/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  final List<int> data = [3, 18, 45, 16, 2, 5, 7, 9, 10, 1, 16, 11, 4, 8, 11, 16, 32, 56];
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  print(sortKecilBesar(data));
+  print(sortBesarKecil(data));
+  print(findMean(data));
+  print(findMedian(data));
+  print(findMode(data));
+}
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+sortKecilBesar(List data) {
+  data.sort();
+  return data;
+}
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+sortBesarKecil(List<int> data) {
+  data.sort((b, a) => a.compareTo(b));
+  return data;
+}
+
+findMean(List<int> data) {
+  var sum = 0;
+  for (var i in data) {
+    sum += i;
+  }
+  // print(sum);
+  // print(data.length);
+  return sum / data.length;
+}
+
+findMedian(List<int> data) {
+  data.sort();
+  var indexMedian = (data.length + 1) / 2;
+  return data[indexMedian.toInt()];
+}
+
+findMode(List<int> data) {
+  Map sebuahMap = {};
+  for (var element in data) {
+    if (sebuahMap[element] == null) {
+      sebuahMap[element] = 0;
+    }
+    sebuahMap[element] += 1;
+  }
+  print(sebuahMap);
+  var modusKey = 0;
+  var modusValue = 0;
+  sebuahMap.forEach((key, value) {
+    if (value >= modusValue) {
+      print('executed!');
+      modusValue = value;
+      modusKey = key;
+    }
   });
+  return modusKey;
 }
